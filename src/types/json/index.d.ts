c11a77ff1fd6760efebc34741d08cf66411418b4
hyroky867 <handm755@gmail.com>
2023-02-18 15:45:06 +0900

    feat: add color data

--- src/types/json/index.d.ts
+++ src/types/json/index.d.ts
@@ -1,6 +1,11 @@
-import { RecipeType } from '..';
+import { RecipeType, ColorType } from '..';
 
 declare module '*/recipe.json' {
   const value: RecipeType[];
+  export = value;
+}
+
+declare module '*/color-data.json' {
+  const value: ColorType[];
   export = value;
 }
