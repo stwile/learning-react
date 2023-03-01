7458bb7f87b9dc13c1435828f28e742158f038a8
hyroky867 <handm755@gmail.com>
2023-03-02 00:44:55 +0900

    section: 6.3.2 ~ 6.3.3

--- src/components/AddColorForm.tsx
+++ src/components/AddColorForm.tsx
@@ -1,31 +1,35 @@
-import { useRef } from 'react';
+import { useInput } from '../hooks/useInput';
 
 type Props = {
   onNewColor: (title: string, color: string) => void;
 };
 
 const AddColorForm = ({ onNewColor }: Props) => {
-  const txtTile = useRef<HTMLInputElement>(null);
-  const hexColor = useRef<HTMLInputElement>(null);
+  const { props: titleProps, reset: resetTitle } = useInput('');
+  const { props: colorProps, reset: resetColor } = useInput('#000000');
 
   const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const title = txtTile.current?.value ?? '';
-    const color = hexColor.current?.value ?? '';
-
-    onNewColor(title, color);
-    if (txtTile.current !== null) {
-      txtTile.current.value = '';
-    }
-    if (hexColor.current !== null) {
-      hexColor.current.value = '';
-    }
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTile} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        value={titleProps.value}
+        onChange={titleProps.onChange}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input
+        value={colorProps.value}
+        onChange={colorProps.onChange}
+        type="color"
+        required
+      />
       <button type="submit">ADD</button>
     </form>
   );
