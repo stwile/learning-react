ab785dcb754f65ba81b3e94e2f3043581041a24e
hyroky867 <handm755@gmail.com>
2023-07-27 20:23:18 +0900

    feat: section 8.3

--- src/components/GithubUser.tsx
+++ src/components/GithubUser.tsx
@@ -4,8 +4,42 @@ type Props = {
   login: string;
 };
 
+type Response = {
+  name: string;
+  login: string;
+  avatar_url: string;
+  location: string;
+};
+
+// ローカルストレージからデータを読み込む
+const loadJSON = <T,>(key: string): T | null => {
+  if (!key) return null;
+
+  const json = localStorage.getItem(key);
+  if (json === null) return null;
+
+  return JSON.parse(json) as T;
+};
+
+// ローカルストレージにデータを書き込む
+const saveJSON = <T,>(key: string, data: T): void =>
+  localStorage.setItem(key, JSON.stringify(data));
+
 const GithubUser = ({ login }: Props) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(loadJSON<Response>(`user:${login}`));
+
+  useEffect(() => {
+    if (!login) return;
+    if (data === null) return;
+    if (data.login === login) return;
+    const { name, avatar_url: avatarUrl, location } = data;
+    saveJSON<Response>(`user:${login}`, {
+      name,
+      login,
+      avatar_url: avatarUrl,
+      location,
+    });
+  }, [data]);
 
   useEffect(() => {
     if (!login) return;
