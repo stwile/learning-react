a064836262867165a86173818e6f7e8112ac672d
hyroky867 <handm755@gmail.com>
2023-08-09 21:45:20 +0900

    section: 11.4

--- src/features/CompanyWebsite/components/App.tsx
+++ src/features/CompanyWebsite/components/App.tsx
@@ -1,27 +1,37 @@
-import { Route, Routes } from 'react-router';
+import { useRoutes } from 'react-router-dom';
 
 import { About } from './About';
 import { Contact } from './Contact';
 import { Events } from './Events';
 import { History } from './History';
 import { Home } from './Home';
+import { Location } from './Location';
 import { Products } from './Products';
 import { Services } from './Services';
 import { Whoops404 } from './Whoops404';
 
-const App = () => (
-  <div>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="services" element={<Services />} />
-      <Route path="history" element={<History />} />
-      <Route path="events" element={<Events />} />
-      <Route path="products" element={<Products />} />
-      <Route path="contact" element={<Contact />} />
-      <Route path="*" element={<Whoops404 />} />a
-    </Routes>
-  </div>
-);
+const App = () =>
+  useRoutes([
+    { path: '/', element: <Home /> },
+    {
+      path: 'about',
+      element: <About />,
+      children: [
+        {
+          path: 'services',
+          element: <Services />,
+        },
+        { path: 'history', element: <History /> },
+        {
+          path: 'location',
+          element: <Location />,
+        },
+      ],
+    },
+    { path: 'events', element: <Events /> },
+    { path: 'products', element: <Products /> },
+    { path: 'contact', element: <Contact /> },
+    { path: '*', element: <Whoops404 /> },
+  ]);
 
 export { App };
