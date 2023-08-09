8146163e7c9633d11ce4469089928b45ecab7d90
hyroky867 <handm755@gmail.com>
2023-08-09 21:53:58 +0900

    section: 11.5

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { App } from './features/CompanyWebsite/components/App';
+import { App } from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
